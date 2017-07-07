ca4e6d95f79f6c65e9588d50b2caffcde04688d4
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-07-07 16:18:55 +0100

    Working on onClick

--- app/src/main/java/com/example/user/contractawardlist/ContractsActivity.java
+++ app/src/main/java/com/example/user/contractawardlist/ContractsActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,6 +22,21 @@ public class ContractsActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(contractsAdapter);
+    }
+
+    public void getContract(View listItem){
+        Contract movie = (Contract) listItem.getTag();
+        StringBuilder sb = new StringBuilder();
+        sb.append("You clicked");
+        sb.append(movie.getTitle());
+
+        Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show();
+//        Snackbar snackbar = Snackbar.make(listItem, sb.toString(), Snackbar.LENGTH_LONG);
+//
+//        //snackbar.setAction("Say Hello", this);
+//
+//        snackbar.show();
+
     }
 
 }
